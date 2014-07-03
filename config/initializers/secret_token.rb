f73910731ec3e8d6778d1626246808b0f04daa23
Shaun Koo <shaunktw@gmail.com>
2014-07-03 10:40:28 -0400

    Reinitialized

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Trackmetrics::Application.config.secret_key_base = '8957910b25f2c53af44a9f2d46a672ef8e7248eb7ca1100de7c445c2942cdff5f08409cf81e8a90e6fbd477d0454980aeb81a1ba6079a0e4fc1b96fa06d2f52a'
