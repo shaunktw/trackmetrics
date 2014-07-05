b717d7381c19c56fb7bb6bdda94d6837b26b1c9c
Shaun Koo <shaunktw@gmail.com>
2014-07-05 10:41:31 -0400

    Added verification in domain model and styled view

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -1,7 +1,18 @@
 class Domain
   include Mongoid::Document
+  
   field :name, type: String
   field :url, type: String
+  field :active, type: Boolean, default: false
+  field :verification_token, type: String
 
   belongs_to :user
+
+  before_create :generate_verification_token
+
+  protected
+
+  def generate_verification_token
+    self.verification_token = SecureRandom.hex(30)
+  end
 end
