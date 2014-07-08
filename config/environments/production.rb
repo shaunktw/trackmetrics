2eda22de469f27fbc1bb5af1f3fed204a526cc10
Shaun Koo <shaunktw@gmail.com>
2014-07-07 21:44:17 -0400

    Modified sendgrid to include production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,5 @@ Trackmetrics::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.action_mailer.default_url_options = { host: 'trackmetrics.herokuapp.com' }
 end
