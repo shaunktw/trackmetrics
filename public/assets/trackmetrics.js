921d72c2ad5b7de4a4207d41b6282bd2b2bb53c3
Shaun Koo <shaunktw@gmail.com>
2014-07-30 07:09:42 -0400

    Updated trackmetrics.js

--- public/assets/trackmetrics.js
+++ public/assets/trackmetrics.js
@@ -5,7 +5,7 @@ var Trackmetrics = {
   },
   send: function(name, data) {
     var _bm_request = new XMLHttpRequest();
-    _bm_request.open("POST", "http://localhost:3000/api/v1/events.json?auth_token=" + this.authToken, true);
+    _bm_request.open("POST", "http://trackmetrics.herokuapp.com/api/v1/events.json?auth_token=" + this.authToken, true);
     _bm_request.setRequestHeader('Content-Type', 'application/json');
     _bm_request.onreadystatechange = function() {
       // this function runs when the Ajax request changes state.
